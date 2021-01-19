573d10f0615dbedc44b4e690d45aefdd7e9573b9
Scotnay <44365822+Scotnay@users.noreply.github.com>
2021-01-19 16:07:56 +0000

    Added ATM Plugin

---
+++ atm/sv_plugin.lua
@@ -0,0 +1,28 @@
+local PLUGIN = PLUGIN
+
+
+function PLUGIN:SaveATM()
+
+  for _, v in ipairs(ents.FindByClass("ix_atm")) do
+    local data = {}
+
+    data[#data + 1] = {
+      v:GetPos(),
+      v:GetAngles()
+    }
+
+    ix.data.Set("atmMachines", data)
+
+  end
+end
+
+function PLUGIN:LoadATM()
+
+  for _, v in ipairs(ix.data.Get("atmMachines") or {} ) do
+    local atm = ents.Create("ix_atm")
+
+    atm:SetPos(v[1])
+    atm:SetAngles(v[2])
+    atm:Spawn()
+  end
+end
